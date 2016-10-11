85569a4cafe1ebf2aaa2fb90dd014b74c926a58f
Pierre Chifflier <chifflier@wzdftpd.net>
2016-10-11 19:18:48 +0200

    Parse TLS records in two steps: first envelope, then data

--- src/main.rs
+++ src/main.rs
@@ -15,37 +15,53 @@ use pnet::packet::tcp::TcpPacket;
 use pnet::packet::ip::IpNextHeaderProtocols;
 
 extern crate tls_parser;
-use tls_parser::tls::{TlsMessage,TlsPlaintext,TlsMessageHandshake,tls_parser_many};
+use tls_parser::tls::{TlsMessage,TlsPlaintext,TlsMessageHandshake,parse_tls_raw_record,parse_tls_raw_record_as_plaintext};
 use tls_parser::tls_ciphers::TlsCipherSuite;
 use tls_parser::tls_extensions::parse_tls_extensions;
 
 extern crate nom;
 use nom::IResult;
 
-fn handle_parsed_data(v:&Vec<TlsPlaintext>) {
-    for ref record in v {
-        println!("{:?}", record);
-        for msg in &record.msg {
-            match *msg {
-                TlsMessage::Handshake(ref m) => {
-                    match *m {
-                        TlsMessageHandshake::ClientHello(ref content) => {
-                            let blah = parse_tls_extensions(content.ext.unwrap_or(b""));
-                            println!("ext {:?}", blah);
-                        },
-                        TlsMessageHandshake::ServerHello(ref content) => {
-                            match TlsCipherSuite::from_id(content.cipher) {
-                                Some(c) => println!("Selected cipher: {:?}", c),
-                                _ => println!("Unknown ciphe 0x{:x}", content.cipher),
-                            };
-                            let blah = parse_tls_extensions(content.ext.unwrap_or(b""));
-                            println!("ext {:?}", blah);
-                        },
-                        _ => (),
-                    }
-                },
-                _ => (),
-            }
+fn handle_parsed_tls_record(record: &TlsPlaintext) {
+    debug!("plaintext: {:?}", record);
+    for msg in &record.msg {
+        match *msg {
+            TlsMessage::Handshake(ref m) => {
+                match *m {
+                    TlsMessageHandshake::ClientHello(ref content) => {
+                        let blah = parse_tls_extensions(content.ext.unwrap_or(b""));
+                        debug!("ext {:?}", blah);
+                    },
+                    TlsMessageHandshake::ServerHello(ref content) => {
+                        match TlsCipherSuite::from_id(content.cipher) {
+                            Some(c) => info!("Selected cipher: {:?}", c),
+                            _ => info!("Unknown cipher 0x{:x}", content.cipher),
+                        };
+                        let ext = parse_tls_extensions(content.ext.unwrap_or(b""));
+                        debug!("ext {:?}", ext);
+                    },
+                    _ => (),
+                }
+            },
+            _ => (),
+        }
+    }
+}
+
+fn parse_data_as_tls(i: &[u8]) {
+    let mut cur_i = i;
+
+    while cur_i.len() > 0 {
+        let res_raw = parse_tls_raw_record(cur_i);
+        debug!("raw: {:?}",res_raw);
+        match res_raw {
+            IResult::Done(rem, ref r) => {
+                let plaintext = parse_tls_raw_record_as_plaintext(r).unwrap();
+                handle_parsed_tls_record(&plaintext);
+                cur_i = rem;
+            },
+            IResult::Incomplete(_) => warn!("Fragmentation required ? {:?}", res_raw),
+            IResult::Error(e) => warn!("Parsing failed: {:?}",e),
         }
     }
 }
@@ -62,16 +78,7 @@ fn callback(ds: usize, packet: pcap::Packet) {
             Some(ref tcp) => {
                 //debug!("tcp payload: {:?}", tcp.payload());
 
-                let d = tls_parser_many(tcp.payload());
-                match d {
-                    IResult::Done(rem,ref v) => {
-                        handle_parsed_data(v);
-                        if rem.len() > 0 {
-                            println!("** unparsed ** {:?}",rem);
-                        }
-                    },
-                    _ =>  println!("parsing failed: {:?}", d),
-                }
+                parse_data_as_tls(tcp.payload());
             },
             None => (), // not a TCP packet, ignore
         }
