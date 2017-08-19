9e17756a9ea3d3ae18a35aece11e89f22f1dce8e
Pierre Chifflier <chifflier@wzdftpd.net>
2017-08-19 10:48:03 +0200

    Split sessions by 5-tuple hash, and assign a parser for each session

--- src/main.rs
+++ src/main.rs
@@ -21,17 +21,35 @@ use pnet::packet::ip::IpNextHeaderProtocols;
 
 #[macro_use]
 extern crate nom;
+use nom::HexDisplay;
 
 extern crate rusticata;
-use rusticata::IPsecParser;
-use rusticata::NtpParser;
-use rusticata::TlsParser;
 use rusticata::RParser;
+
+mod registry;
+use registry::ParserRegistry;
+
+mod five_tuple;
+use five_tuple::{FiveTuple,ToFiveTuple};
 
 mod pcap_nflog;
 use pcap_nflog::get_data_nflog;
 
-fn parse_data_as(parser: &mut RParser, i: &[u8])
+struct GlobalState {
+    registry: ParserRegistry,
+    sessions: HashMap<FiveTuple, Box<RParser>>,
+}
+
+impl GlobalState {
+    pub fn new() -> GlobalState {
+        GlobalState{
+            registry: ParserRegistry::new(),
+            sessions: HashMap::new(),
+        }
+    }
+}
+
+fn parse_data_as(parser: &mut RParser, i: &[u8], direction: u8)
 {
     if i.len() == 0 {
         return;
@@ -39,17 +57,38 @@ fn parse_data_as(parser: &mut RParser, i: &[u8])
 
     // let mut state = TLS_STATE.lock().unwrap();
     // state.parse_tcp_level(i);
-    parser.parse(i,0);
+    parser.parse(i, direction);
 }
 
-fn callback(data:&[u8], p: &mut RParser)
+fn callback(data:&[u8], ptype: &String, globalstate: &mut GlobalState)
 {
     debug!("----------------------------------------");
-    debug!("raw packet: {:?}", data);
+    debug!("raw packet:\n{}", data.to_hex(16));
 
     //let ref ether = EthernetPacket::new(packet.data).unwrap();
     let ref ipv4 = Ipv4Packet::new(data).unwrap();
     // debug!("next level proto: {:?}", ipv4.get_next_level_protocol());
+
+    let mut five_t = ipv4.get_five_tuple();
+    let mut direction : u8 = 0;
+    debug!("5T: {:?}", five_t);
+
+    if !globalstate.sessions.contains_key(&five_t) {
+        // not found, lookup reverse hash
+        let rev_five_t = five_t.get_reverse();
+        debug!("rev 5T: {:?}", rev_five_t);
+        if globalstate.sessions.contains_key(&rev_five_t) {
+            debug!("found reverse hash");
+            five_t = rev_five_t;
+            direction = 1;
+        } else {
+            debug!("Creating new session");
+            globalstate.sessions.insert(five_t.clone(), globalstate.registry.create(ptype).unwrap());
+        }
+    }
+    let pp  = globalstate.sessions.get_mut(&five_t).unwrap();
+    let p = &mut (**pp);
+
     if ipv4.get_next_level_protocol() == IpNextHeaderProtocols::Tcp {
         match TcpPacket::new(ipv4.payload()) {
             Some(ref tcp) => {
@@ -67,7 +106,7 @@ fn callback(data:&[u8], p: &mut RParser)
                 };
 
                 // XXX check if data is indeed TLS/...
-                parse_data_as(p, payload);
+                parse_data_as(p, payload, direction);
             },
             None => (), // not a TCP packet, ignore
         }
@@ -87,7 +126,7 @@ fn callback(data:&[u8], p: &mut RParser)
                     payload = &payload[0..new_len];
                 };
                 // XXX check if data is indeed IPsec/NTP/...
-                parse_data_as(p, payload);
+                parse_data_as(p, payload, direction);
             },
             None => (), // not a UDP packet, ignore
         }
@@ -125,28 +164,21 @@ fn main() {
         ap.parse_args_or_exit();
     }
 
-    let mut m : HashMap<String,Box<RParser>> = HashMap::new();
-    m.insert("ipsec".to_string(), Box::new(IPsecParser::new(b"IPsec")));
-    m.insert("ntp".to_string(), Box::new(NtpParser::new(b"NTP")));
-    m.insert("tls".to_string(), Box::new(TlsParser::new(b"TLS")));
+    let mut globalstate = GlobalState::new();
 
-    if let Some(mut p) = m.get_mut(&parser) {
-        let mut cap = pcap::Capture::from_file(filename).unwrap();
-        println!("datalink: {:?}",cap.get_datalink());
+    let mut cap = pcap::Capture::from_file(filename).unwrap();
+    println!("datalink: {:?}",cap.get_datalink());
 
-        // See http://www.tcpdump.org/linktypes.html
-        let get_data = match cap.get_datalink() {
-            pcap::Linktype(1) => get_data_ethernet,
-            pcap::Linktype(113) => get_data_linux_cooked,
-            pcap::Linktype(239) => get_data_nflog,
-            _ => panic!("unsupported data link type"),
-        };
+    // See http://www.tcpdump.org/linktypes.html
+    let get_data = match cap.get_datalink() {
+        pcap::Linktype(1) => get_data_ethernet,
+        pcap::Linktype(113) => get_data_linux_cooked,
+        pcap::Linktype(239) => get_data_nflog,
+        _ => panic!("unsupported data link type"),
+    };
 
-        while let Ok(packet) = cap.next() {
-            let data = get_data(&packet);
-            callback(data,&mut (**p));
-        }
-    } else {
-        println!("Unknown parser");
+    while let Ok(packet) = cap.next() {
+        let data = get_data(&packet);
+        callback(data, &parser, &mut globalstate);
     }
 }
