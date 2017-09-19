8b443efdb3dd26e71464a6e971d2889285450a5d
Pierre Chifflier <chifflier@wzdftpd.net>
2017-09-19 07:51:23 +0200

    Add support for Radius parser

--- src/registry.rs
+++ src/registry.rs
@@ -1,20 +1,22 @@
-use rusticata::{RParser,IPsecParser,NtpParser,SSHParser,TlsParser};
+use rusticata::{RParser,IPsecParser,NtpParser,RadiusParser,SSHParser,TlsParser};
 
 pub struct ParserRegistry {}
 
 impl ParserRegistry {
     pub fn create_ipsec<'a>() -> IPsecParser<'a> { IPsecParser::new(b"IPsec") }
     pub fn create_ntp<'a>() -> NtpParser<'a> { NtpParser::new(b"NTP") }
+    pub fn create_radius<'a>() -> RadiusParser<'a> { RadiusParser::new(b"Radius") }
     pub fn create_ssh<'a>() -> SSHParser<'a> { SSHParser::new(b"SSH") }
     pub fn create_tls<'a>() -> TlsParser<'a> { TlsParser::new(b"TLS") }
 
     pub fn create<'a>(&self, s: &String) -> Result<Box<RParser>,&'static str> {
         match s.as_ref() {
-            "ipsec" => Ok(Box::new(Self::create_ipsec())),
-            "ntp"   => Ok(Box::new(Self::create_ntp())),
-            "ssh"   => Ok(Box::new(Self::create_ssh())),
-            "tls"   => Ok(Box::new(Self::create_tls())),
-            _       => Err("unknown parser type")
+            "ipsec"  => Ok(Box::new(Self::create_ipsec())),
+            "ntp"    => Ok(Box::new(Self::create_ntp())),
+            "radius" => Ok(Box::new(Self::create_radius())),
+            "ssh"    => Ok(Box::new(Self::create_ssh())),
+            "tls"    => Ok(Box::new(Self::create_tls())),
+            _        => Err("unknown parser type")
         }
     }
 
